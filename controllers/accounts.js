3648f254b1a938c42980d1fa8b56d661944529b6
Ionut Moraru <morion4000@gmail.com>
2016-10-17 15:03:58 +0300

    Accounts controller

---
+++ controllers/accounts.js
@@ -0,0 +1,81 @@
+var account = require('./../models').account,
+  errors = require('./../errors'),
+  config = require('./../config');
+
+var Accounts = function() {
+
+  var collection = function(req, res, next) {
+    account.findAll(req, function(err, result) {
+      if (err) {
+        return next(err);
+      }
+
+      res.send(result);
+    });
+  };
+
+  var create = function(req, res, next) {
+    req.body.user_id = req.user.sub;
+
+    account.create(req.body, function(err, result) {
+      if (err) {
+        return next(err);
+      }
+
+      res.status(201);
+      res.send();
+    });
+  };
+
+  var retrieve = function(req, res, next) {
+    account.find(req, function(err, result) {
+      if (err) {
+        return next(err);
+      }
+
+      if (!result) {
+        return next(new errors.not_found('Account not found'));
+      }
+
+      res.send(result);
+    });
+  };
+
+  var update = function(req, res, next) {
+    account.update(req.body, {
+      id: req.id,
+      user_id: req.user.sub
+    }, function(err, result) {
+      if (err) {
+        return next(err);
+      }
+
+      res.status(204);
+      res.send();
+    });
+  };
+
+  var remove = function(req, res, next) {
+    account.destroy({
+      id: req.id,
+      user_id: req.user.sub
+    }, function(err, result) {
+      if (err) {
+        return next(err);
+      }
+
+      res.status(204);
+      res.send();
+    });
+  };
+
+  return {
+    collection: collection,
+    create: create,
+    retrieve: retrieve,
+    update: update,
+    remove: remove
+  }
+};
+
+module.exports = Accounts();
