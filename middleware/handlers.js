f31d17097eff924aea33364c6e73b051f56c1e8b
Ionut Moraru <morion4000@gmail.com>
2016-10-17 12:25:57 +0300

    Better error handling

--- middleware/handlers.js
+++ middleware/handlers.js
@@ -1,13 +1,27 @@
-var config = require('./../config');
+var _ = require('underscore'), 
+  config = require('./../config');
 
 var errors = function(err, req, res, next) {
-  var error_code = err.code || 500,
-    error_message = err.message || 'Internal server error';
+  var error_code = 500,
+    error_message = err.message || 'Internal server error',
+    environment = config.env;
 
-  switch (config.env) {
+  if (_.has(err, 'code') && _.isNumber(err.code)) {
+    error_code = err.code;
+  }
+
+  if (_.has(err, 'status') && _.isNumber(err.status)) {
+    error_code = err.status;
+  }
+
+  switch (environment) {
     case 'production':
+      console.log(req.method, req.url, err);
+      break;
+
     case 'test':
       console.log(req.method, req.url, err);
+      break;
 
     case 'development':
       if (error_code === 500) {
@@ -17,11 +31,11 @@ var errors = function(err, req, res, next) {
       break;
   }
 
-  res.send(error_code, {
+  res.status(error_code).send({
     code: error_code,
     message: error_message
   });
-}
+};
 
 var raw_body = function(req, res, next) {
   var data = '';
