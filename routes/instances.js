d3ae3ed0e4f1609611fbc087e0964dbfa4f123f5
Ionut Moraru <morion4000@gmail.com>
2016-10-17 13:23:55 +0300

    Fixed the route definitions for Instances

--- routes/instances.js
+++ routes/instances.js
@@ -4,11 +4,11 @@ var routers = require('./../middleware').routers,
 module.exports = function(app, router) {
   router.param('instance_id', routers.param);
 
-  router.post('/instances', routers.bodyCleanup, instances.collection);
-  router.get('/instances/:instance_id', instances.collection);
-  router.put('/instances/:instance_id', routers.bodyCleanup, instances.collection);
-  router.delete('/instances/:instance_id', instances.collection);
+  router.post('/instances', routers.bodyCleanup, instances.create);
+  router.get('/instances/:instance_id', instances.retrieve);
+  router.put('/instances/:instance_id', routers.bodyCleanup, instances.update);
+  router.delete('/instances/:instance_id', instances.remove);
 
-  // CollectionsS
+  // Collections
   router.get('/instances', routers.filters, instances.collection);
 };
