d6da30f9fdb0b6ba6afe947a5d94ce3a3deef67c
Ionut Moraru <morion4000@gmail.com>
2018-09-05 18:36:54 +0300

    New workers

---
+++ workers/rancher_sync_hosts_status.js
@@ -0,0 +1,73 @@
+var rancher = require('./../services').Rancher();
+var config = require('./../config');
+
+var host_model = require('./../models').host.model;
+var account_model = require('./../models').account.model;
+
+function find_host_in_hosts(host, hosts) {
+  for (var i = 0, l = hosts.length; i < l; i++) {
+    if (host.id == hosts[i].internal_id) {
+      return hosts[i];
+    }
+  }
+
+  return null;
+}
+
+rancher.hosts.query(function(err, message, body) {
+  var data = JSON.parse(body);
+  var hosts = data.data;
+  var result = [];
+
+  for (var i = 0, l = hosts.length; i < l; i++) {
+    var host = hosts[i];
+
+    if (host.labels.account) {
+      console.log('found rancher host', host.id);
+
+      result.push(host);
+    }
+  }
+
+  console.log('rancher hosts', result.length);
+
+  host_model.findAll({})
+    .then(function(data) {
+      var hosts = data;
+
+      for (var i = 0, l = result.length; i < l; i++) {
+        var host = result[i];
+
+        var db_host = find_host_in_hosts(host, hosts);
+
+        if (db_host) {
+          var status = 'stopped';
+
+          if (host.agentState !== 'disconnected') {
+            status = 'started';
+          }
+
+          if (db_host.status == status) {
+            console.log('skipping host in mysql', host.hostname);
+
+            continue;
+          }
+
+          console.log('updating host in mysql', host.hostname);
+
+          host_model.update({
+              status: status,
+            }, {
+              where: {
+                id: db_host.id
+              }
+            })
+            .then(console.log)
+            .catch(console.error);
+        } else {
+          console.log('removed host', host.hostname);
+        }
+      }
+    })
+    .catch(console.error);
+});
--- workers/rancher_sync_miners.js
+++ workers/rancher_sync_miners.js
@@ -5,94 +5,87 @@ var miner_model = require('./../models').miner.model;
 var host_model = require('./../models').host.model;
 var account_model = require('./../models').account.model;
 
-rancher.services.query(function(err, message, body) {
-  var data = JSON.parse(body);
-  var services = data.data;
+miner_model.findAll({
+    include: [{
+      model: host_model
+    }]
+  })
+  .then(function(data) {
+    var miners = data;
 
-  console.log('found services', services.length);
+    console.log('found miners', miners.length);
 
-  miner_model.findAll({
-      include: [{
-        model: host_model
-      }]
-    })
-    .then(function(data) {
-      var miners = data;
+    for (var i = 0, l = miners.length; i < l; i++) {
+      var miner = miners[i];
 
-      console.log('found miners', miners.length);
+      if (!miner.internal_id) {
+        // TODO: Make sure is not already deployed
+        console.log('deploying miner', miner.id);
 
-      for (var i = 0, l = miners.length; i < l; i++) {
-        var miner = miners[i];
+        var req = {
+          body: {
+            id: miner.id,
+            name: miner.name,
+            user_id: miner.user_id,
+            server_port: miner.server_port,
+            mining_pool_url: miner.mining_pool_url,
+            domain: miner.domain,
+            wallet: miner.wallet,
+            wallet_secret_url: miner.wallet_secret_url,
+            terminal_workers_type: miner.terminal_workers_type,
+            terminal_workers_cpu_max: miner.terminal_workers_cpu_max,
+            image_uuid: miner.image_uuid,
+            host_id: miner.Host.internal_id,
+            host_id2: miner.Host.id,
+            host_account_id: miner.Host.account_id,
+          }
+        };
 
-        if (!miner.internal_id) {
-          // TODO: Make sure is not already deployed
-          console.log('deploying miner', miner.id);
+        (function(_req) {
+          account_model.findOne({
+              where: {
+                id: _req.body.host_account_id
+              }
+            })
+            .then(function(account) {
+              if (!account) return;
 
-          var req = {
-            body: {
-              id: miner.id,
-              name: miner.name,
-              user_id: miner.user_id,
-              server_port: miner.server_port,
-              mining_pool_url: miner.mining_pool_url,
-              domain: miner.domain,
-              wallet: miner.wallet,
-              wallet_secret_url: miner.wallet_secret_url,
-              terminal_workers_type: miner.terminal_workers_type,
-              terminal_workers_cpu_max: miner.terminal_workers_cpu_max,
-              image_uuid: miner.image_uuid,
-              host_id: miner.Host.internal_id,
-              host_id2: miner.Host.id,
-              host_account_id: miner.Host.account_id,
-            }
-          };
+              console.log('account', account.internal_id);
 
-          (function(_req) {
-            account_model.findOne({
-                where: {
-                  id: _req.body.host_account_id
+              _req.body.stack_id = account.internal_id;
+
+              rancher.services.create(_req, {}, function(err) {
+                if (err) {
+                  console.err(err);
+                  return;
                 }
-              })
-              .then(function(account) {
-                if (!account) return;
 
-                console.log('account', account.internal_id);
+                console.log('deployed', _req.rancher_service_id);
 
-                _req.body.stack_id = account.internal_id;
+                miner_model.update({
+                    status: 'started',
+                    internal_id: _req.rancher_service_id
+                  }, {
+                    where: {
+                      id: _req.body.id
+                    }
+                  })
+                  .then(console.log)
+                  .catch(console.error);
 
-                rancher.services.create(_req, {}, function(err) {
-                  if (err) {
-                    console.err(err);
-                    return;
-                  }
-
-                  console.log('deployed', _req.rancher_service_id);
-
-                  miner_model.update({
-                      status: 'started',
-                      internal_id: _req.rancher_service_id
-                    }, {
-                      where: {
-                        id: _req.body.id
-                      }
-                    })
-                    .then(console.log)
-                    .catch(console.error);
-
-                  host_model.update({
-                      deployed: '1'
-                    }, {
-                      where: {
-                        id: _req.body.host_id2
-                      }
-                    })
-                    .then(console.log)
-                    .catch(console.error);
-                });
+                host_model.update({
+                    deployed: '1'
+                  }, {
+                    where: {
+                      id: _req.body.host_id2
+                    }
+                  })
+                  .then(console.log)
+                  .catch(console.error);
               });
-          })(req);
-        }
+            });
+        })(req);
       }
-    })
-    .catch(console.error);
-});
+    }
+  })
+  .catch(console.error);
