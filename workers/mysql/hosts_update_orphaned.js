40d6970f02c61b1549ef0a005f9147bb10236f22
Ionut Moraru <morion4000@gmail.com>
2018-10-09 15:43:24 +0300

    new worker

---
+++ workers/mysql/hosts_update_orphaned.js
@@ -0,0 +1,28 @@
+var sequelize = require('./../../models').sequelize;
+var config = require('./../../config');
+
+var host_model = require('./../../models').host.model;
+
+sequelize.query('SELECT hosts.id, hosts.deployed FROM hosts LEFT JOIN miners ON hosts.id = miners.host_id WHERE miners.host_id IS NULL', {
+  model: host_model
+}).then(function(hosts) {
+  for (var i = 0, l = hosts.length; i < l; i++) {
+    var host = hosts[i];
+
+    if (host.deployed != '0') {
+      console.log('undeployed', host.id);
+
+      host_model.update({
+          deployed: '0'
+        }, {
+          where: {
+            id: host.id
+          }
+        })
+        .then(console.log)
+        .catch(console.error);
+    } else {
+      console.log('skipped', host.id);
+    }
+  }
+});
