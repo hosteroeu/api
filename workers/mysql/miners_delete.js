eb1ed95a191fdf01814c9ec071b5c5a7b79d2e09
Ionut Moraru <morion4000@gmail.com>
2019-01-19 16:39:24 +0200

    Update miners_delete.js

--- workers/mysql/miners_delete.js
+++ workers/mysql/miners_delete.js
@@ -1,9 +1,11 @@
+var moment = require('moment');
 var rancher = require('./../../services').Rancher();
 var config = require('./../../config');
 
 var miner_model = require('./../../models').miner.model;
 var host_model = require('./../../models').host.model;
 var account_model = require('./../../models').account.model;
+var log_model = require('./../../models').log.model;
 
 function find_miner_in_miners(miner, miners) {
   for (var i = 0, l = miners.length; i < l; i++) {
@@ -15,7 +17,19 @@ function find_miner_in_miners(miner, miners) {
   return null;
 }
 
-miner_model.findAll({})
+miner_model.findAll({
+    where: {
+      created_at: {
+        $lte: moment().subtract(10, 'minutes').toDate()
+      }
+    },
+    include: [{
+      model: host_model,
+      include: [{
+        model: account_model
+      }]
+    }]
+  })
   .then(function(data) {
     var miners = data;
 
@@ -47,6 +61,16 @@ miner_model.findAll({})
             })
             .then(console.log)
             .catch(console.error);
+
+          log_model.create({
+            user_id: miner.Host.Account.user_id,
+            account_id: miner.Host.Account.id,
+            entity: 'miner',
+            entity_id: miner.id,
+            event: 'delete',
+            message: 'Removed a miner',
+            extra_message: JSON.stringify(miner)
+          });
         }
       }
     });
