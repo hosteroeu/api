3d67cb2cbf75006f4392ac108375af66820de52b
Ionut Moraru <morion4000@gmail.com>
2019-02-02 23:49:35 +0200

    Update accounts_update.js

--- workers/mysql/accounts_update.js
+++ workers/mysql/accounts_update.js
@@ -6,6 +6,7 @@ var payment_model = require('./../../models').payment.model;
 var account_model = require('./../../models').account.model;
 var log_model = require('./../../models').log.model;
 
+// TODO: Replace with moment
 var d = new Date();
 var m = d.getMonth();
 d.setMonth(d.getMonth() - 1);
@@ -17,7 +18,9 @@ d.setMilliseconds(0);
 
 var last_month_date = d;
 
-account_model.findAll().then(function(data) {
+account_model.findAll({
+  logging: false
+}).then(function(data) {
   var accounts = data;
 
   for (var i = 0, l = accounts.length; i < l; i++) {
@@ -35,101 +38,102 @@ account_model.findAll().then(function(data) {
           account_id: _account.id
         },
         limit: 1,
-        order: [['created_at', 'DESC']]
-      }).then(function(data) {
-        var payments = data;
-
-        if (payments.length > 0) {
-          console.log('checking payments for', _account.name);
+        order: [
+          ['created_at', 'DESC']
+        ],
+        logging: false
+      }).then(function(payments) {
+        if (payments.length === 0) {
+          return;
         }
 
-        for (var j = 0, k = payments.length; j < k; j++) {
-          var payment = payments[j];
+        console.log('checking payments for', _account.name);
 
-          if (Date.parse(payment.created_at) <= Date.parse(last_month_date)) {
-            if (_account.plan_miners === 1) {
-              continue;
+        var payment = payments[0];
+
+        if (Date.parse(payment.created_at) <= Date.parse(last_month_date)) {
+          if (_account.plan_miners === 1) {
+            return;
+          }
+
+          console.log('payment overdue, downgrading');
+
+          account_model.update({
+            plan_miners: 1
+          }, {
+            where: {
+              id: _account.id
             }
+          }).then(console.log);
 
-            console.log('payment overdue, downgrading');
+          log_model.create({
+            user_id: _account.user_id,
+            account_id: _account.id,
+            entity: 'account',
+            entity_id: _account.id,
+            event: 'update',
+            message: 'Canceled your subscription',
+            extra_message: JSON.stringify({
+              miners: 1
+            })
+          });
 
-            account_model.update({
-              plan_miners: 1
-            }, {
-              where: {
-                id: _account.id
-              }
-            }).then(console.log);
+          if (_account.email) {
+            mailgun.mail.send({
+              to: _account.email,
+              subject: 'Your subscription has been canceled',
+              body: 'Your Hostero subscription was automatically canceled due to non-payment. You are on the free plan now.'
+            }, null, console.log);
+          }
+        } else {
+          var new_plan_miners = 1;
 
-            log_model.create({
-              user_id: _account.user_id,
-              account_id: _account.id,
-              entity: 'account',
-              entity_id: _account.id,
-              event: 'update',
-              message: 'Canceled your subscription',
-              extra_message: JSON.stringify({
-                miners: 1
-              })
-            });
+          switch (payment.amount) {
+            // old plan
+            case '0.99':
+            case '1.99':
+              new_plan_miners = 5;
+              break;
+            case '9.99':
+              new_plan_miners = 20;
+              break;
+            case '49.99':
+              new_plan_miners = 100;
+              break;
+          }
 
-            if (_account.email) {
-              mailgun.mail.send({
-                to: _account.email,
-                subject: 'Your subscription has been canceled',
-                body: 'Your Hostero subscription was automatically canceled due to non-payment. You are on the free plan now.'
-              }, null, console.log);
+          if (_account.plan_miners === new_plan_miners) {
+            return;
+          }
+
+          console.log('payment ok, upgrading');
+
+          account_model.update({
+            plan_miners: new_plan_miners
+          }, {
+            where: {
+              id: _account.id
             }
-          } else {
-            var new_plan_miners = 1;
+          }).then(console.log);
 
-            switch (payment.amount) {
-              // old plan
-              case '0.99':
-              case '1.99':
-                new_plan_miners = 5;
-                break;
-              case '9.99':
-                new_plan_miners = 20;
-                break;
-              case '49.99':
-                new_plan_miners = 100;
-                break;
-            }
+          log_model.create({
+            user_id: _account.user_id,
+            account_id: _account.id,
+            entity: 'account',
+            entity_id: _account.id,
+            event: 'update',
+            message: 'Created a subscription',
+            extra_message: JSON.stringify({
+              miners: new_plan_miners
+            })
+          });
 
-            if (_account.plan_miners === new_plan_miners) {
-              continue;
-            }
-
-            console.log('payment ok, upgrading');
-
-            account_model.update({
-              plan_miners: new_plan_miners
-            }, {
-              where: {
-                id: _account.id
-              }
-            }).then(console.log);
-
-            log_model.create({
-              user_id: _account.user_id,
-              account_id: _account.id,
-              entity: 'account',
-              entity_id: _account.id,
-              event: 'update',
-              message: 'Created a subscription',
-              extra_message: JSON.stringify({
-                miners: new_plan_miners
-              })
-            });
-
-            if (_account.email) {
-              mailgun.mail.send({
-                to: _account.email,
-                subject: 'Your subscription has been created',
-                body: 'Your Hostero subscription has been created following your payment. Thank you for your support!'
-              }, null, console.log);
-            }
+          if (_account.email) {
+            mailgun.mail.send({
+              to: _account.email,
+              subject: 'Your subscription has been created',
+              body: 'Your Hostero subscription has been created following your payment. Thank you for your support!'
+            }, null, console.log);
           }
         }
       });
