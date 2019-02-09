5bf45f4de7e945111d7f907dcb0cc694bb52f99e
Ionut Moraru <morion4000@gmail.com>
2019-02-09 19:45:11 +0200

    Update rancher.js

--- services/rancher.js
+++ services/rancher.js
@@ -95,12 +95,10 @@ var Rancher = function() {
             break;
 
           case 'yenten':
-            create_manifest.launchConfig.command = [
-              '-a yescryptr16 -o ' + req.body.mining_pool_url +
+            create_manifest.launchConfig.command = '-a yescryptr16 -o ' + req.body.mining_pool_url +
               ' -u ' + req.body.wallet +
               ' -p ' + req.body.password +
-              ' -t ' + req.body.threads
-            ];
+              ' -t ' + req.body.threads;
             break;
         }
 
