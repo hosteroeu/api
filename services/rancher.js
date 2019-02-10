b037dc4ae8b0cb2c05c6256c7090bb1d5fb90cb0
Ionut Moraru <morion4000@gmail.com>
2019-02-10 09:16:51 +0200

    yenten

--- services/rancher.js
+++ services/rancher.js
@@ -95,11 +95,13 @@ var Rancher = function() {
             break;
 
           case 'yenten':
-            create_manifest.launchConfig.command = '-a yescryptr16 -o ' + req.body.mining_pool_url +
+            create_manifest.launchConfig.command = [
+              '-a yescryptr16 -o ' + req.body.mining_pool_url +
               ' -u ' + req.body.wallet +
               ' -p ' + req.body.password +
               ' -t ' + req.body.threads +
-              ' -q';
+              ' -q'
+            ];
             break;
         }
 
