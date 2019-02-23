164841e9b44131fe12f885782ba63098ed7a4f9a
Ionut Moraru <morion4000@gmail.com>
2019-02-23 18:04:09 +0200

    Update miner.js

--- utils/miner.js
+++ utils/miner.js
@@ -13,7 +13,7 @@ var Miner = function() {
 
         switch (coin) {
           case 'webdollar':
-            new_miner.image_uuid = 'docker:morion4000/node:1.203.2';
+            new_miner.image_uuid = 'docker:morion4000/node:1.210.6';
             break;
 
           case 'nerva':
