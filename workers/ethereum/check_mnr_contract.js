5d271f853115456787ea81a74bbcc36e0930c313
Ionut Moraru <morion4000@gmail.com>
2019-11-05 13:07:03 +0200

    updated worker

--- workers/ethereum/check_mnr_contract.js
+++ workers/ethereum/check_mnr_contract.js
@@ -2,9 +2,12 @@
 
 var ethers = require('ethers');
 var async = require('async');
+var _ = require('underscore');
 
 var config = require('./../../config');
 var account_model = require('./../../models').account.model;
+var log_model = require('./../../models').account.model;
+var mailgun = require('./../../services').Mailgun();
 
 var BLOCK_START = process.env.BLOCK_START || 8875965;
 var SUPPLY = process.env.SUPPLOY || 11;
@@ -24,10 +27,10 @@ for (var i = 0; i < SUPPLY; i++) {
       // TODO: possibly use tokenByIndex before calling ownerOf
 
       mnr_contract.ownerOf(_i)
-        .then(function(data) {
+        .then(function(address) {
           callback(null, {
-            index: _i,
-            data: data
+            id: _i,
+            address: address
           });
         })
         .catch(callback);
@@ -44,15 +47,77 @@ async.parallel(calls, function(err, results) {
   }
 
   if (results.length > 0) {
+    var grouped = _.groupBy(results, 'address');
+
     account_model.findAll({
       logging: true
     }).then(function(accounts) {
       for (var i = 0, l = accounts.length; i < l; i++) {
         var account = accounts[i];
 
+        // TODO: bonus_eth_address should be unique
         if (account.bonus_eth_address) {
-          console.log(account.bonus_eth_address);
+          console.log('address', account.bonus_eth_address, 'account', account.id);
+
+          var tokens = 0;
+          var bonus_miners_mnr = account.bonus_miners_mnr;
+          var bonus_miners = account.bonus_miners;
+
+          if (grouped.hasOwnProperty(account.bonus_eth_address)) {
+            tokens = grouped[account.bonus_eth_address].length;
+          }
+
+          console.log('tokens', tokens);
+          console.log('bonus miners mnr', bonus_miners_mnr);
+
+          var delta = Math.abs(tokens - bonus_miners_mnr);
+
+          if (tokens > bonus_miners_mnr) {
+            bonus_miners += delta;
+            bonus_miners_mnr = tokens;
+          } else if (tokens < bonus_miners_mnr) {
+            bonus_miners -= delta;
+            bonus_miners_mnr = tokens;
+          } else {
+            continue;
+          }
+
+          console.log('new bonus', bonus_miners, '| old bonus', account.bonus_miners);
+
+          account_model.update({
+            bonus_miners: bonus_miners,
+            bonus_miners_mnr: bonus_miners_mnr
+          }, {
+            where: {
+              id: account.id
+            }
+          }).then(console.log);
+
+          log_model.create({
+            user_id: account.user_id,
+            account_id: account.id,
+            entity: 'account',
+            entity_id: account.id,
+            event: 'update',
+            message: 'Update bonus miners',
+            extra_message: JSON.stringify({
+              bonus_miners: bonus_miners,
+              bonus_miners_mnr: bonus_miners_mnr
+            }),
+            source: 'accounts_update'
+          });
+
+          /*
+          if (account.email) {
+            mailgun.mail.send({
+              to: account.email,
+              subject: 'Your subscription has been canceled',
+              body: 'Your Hostero subscription was automatically canceled due to non-payment. You are on the free plan now.'
+            }, null, console.log);
+          }
+          */
         }
+
       }
     });
   }
