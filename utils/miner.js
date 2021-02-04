cc73f8eca483bd8ded3540542675af4ed058ac93
Ionut Moraru <morion4000@gmail.com>
2021-02-04 09:17:44 +0200

    bumped nerva

--- utils/miner.js
+++ utils/miner.js
@@ -17,7 +17,7 @@ var Miner = function() {
             break;
 
           case 'nerva':
-            new_miner.image_uuid = 'docker:morion4000/nerva:0.1.7.1';
+            new_miner.image_uuid = 'docker:morion4000/nerva:0.1.7.3';
             break;
 
           case 'webchain':
